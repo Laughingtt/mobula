faa956efc92b2836641ae1a0e3561aba73734460
tianjian <jian.tian@udai.work>
2022-03-07 22:52:27 +0800

    update main

---
+++ main.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+@company:UDAI
+@author:tianjian
+@file:main.py
+@time:2022/03/07
+
+"""
+
+
+def main():
+    print("Hello World")
+
+
+if __name__ == '__main__':
+    main()
